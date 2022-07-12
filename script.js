18a57e63563a0323dbf12966b2d18b905adbb4b6
olannrewaju <Bamgbadeolanrewaju@gmail.com>
2022-07-12 08:06:55 -0400

    complete project

--- script.js
+++ script.js
@@ -36,18 +36,12 @@
        const textDisplay = document.querySelector(".text-display")
        
        const restart = () => {
-        /*const restart = Game("x", "o")
-        spaces = []
-        restart.units.forEach((div) => {
-            div.textContent = ""
-        })
-        currentPlayer = ""
-        restart.announcer.textContent = ""*/
         window.location.reload();
     }
     btn3.addEventListener("click", restart)  
 
          const startGame = () => {  
+        // twoPlayers inherits from its prototype (Game)
          const twoPlayers = Game("Player1", "Player2")            
 
          const box = (() => {
@@ -63,9 +57,8 @@
             if (!twoPlayers.spaces[id]){
                 twoPlayers.spaces[id] = twoPlayers.currentPlayer
                 e.target.textContent = twoPlayers.currentPlayer
-                console.log(twoPlayers.spaces)
             }
-
+            //check if player has won after every move
             const playerWon = () => {
                 if (twoPlayers.spaces[0] == twoPlayers.currentPlayer){
                     if (twoPlayers.spaces[1] == twoPlayers.currentPlayer && twoPlayers.spaces[2] == twoPlayers.currentPlayer){
@@ -132,6 +125,7 @@
 
          const againstComputer = () =>{
             const versusComputer = Game("Player", "Computer")
+            let positions = ['0','1','2','3','4','5','6','7','8']
 
             const box = (() => {
             board.style.display = "grid"
@@ -199,53 +193,98 @@
                 textDisplay.style.display = "none"
                 versusComputer.announcer.style.visibility = "visible"
                 versusComputer.announcer.textContent = "Player " + versusComputer.currentPlayer + " has won!"
-               // setTimeout(function(){ versusComputer.restart() }, 1500); 
                 return
             }
             
             if (versusComputer.playerDraw()){
                 textDisplay.style.display = "none"
-                twoPlayers.announcer.textContent = "it is a draw"
-                setTimeout(function(){ versusComputer.restart() }, 1500); 
+                versusComputer.announcer.textContent = "It is a draw" 
                 return
             }
           
             const computerPlay = (() => {
                 
                 let id = Math.floor(Math.random()*positions.length)
-                console.log(id)
-              /*  if (versusComputer.spaces[id] === undefined ){
-                    let target = document.getElementById(id)             
-                    versusComputer.spaces[id] = versusComputer.gameBoard[1]
-                  setTimeout(function(){target.textContent = versusComputer.gameBoard[1]}, 500)  
-                }  else {
-                     id = Math.floor(Math.random()*positions.length)
-                     let target = document.getElementById(id)             
-                    versusComputer.spaces[id] = versusComputer.gameBoard[1]
-                  setTimeout(function(){target.textContent = versusComputer.gameBoard[1]}, 500)
-                }*/
+                
                 while (versusComputer.spaces[id] !== undefined){
                     id = Math.floor(Math.random()*positions.length)
                 }
                 let target = document.getElementById(id)             
                     versusComputer.spaces[id] = versusComputer.gameBoard[1]
                   setTimeout(function(){target.textContent = versusComputer.gameBoard[1]}, 500)
-                console.log(versusComputer.spaces)
+                
+
+                const computerWon = () => {
+                if (versusComputer.spaces[0] == versusComputer.gameBoard[1]){
+                    if (versusComputer.spaces[1] == versusComputer.gameBoard[1] && versusComputer.spaces[2] == versusComputer.gameBoard[1]){
+                        return true
+                    }
+                }
+                if (versusComputer.spaces[0] == versusComputer.gameBoard[1]){
+                    if (versusComputer.spaces[3] == versusComputer.gameBoard[1] && versusComputer.spaces[6] == versusComputer.gameBoard[1]){
+                        return true
+                    }
+                }
+                if (versusComputer.spaces[0] == versusComputer.gameBoard[1]){
+                   if (versusComputer.spaces[4] == versusComputer.gameBoard[1] && versusComputer.spaces[8] == versusComputer.gameBoard[1]){
+                        return true
+                    }
+                }
+                if (versusComputer.spaces[2] == versusComputer.gameBoard[1]){
+                    if (versusComputer.spaces[5] == versusComputer.gameBoard[1] && versusComputer.spaces[8] == versusComputer.gameBoard[1]){
+                        return true
+                    }
+                }
+                if (versusComputer.spaces[2] == versusComputer.gameBoard[1]){
+                    if (versusComputer.spaces[4] == versusComputer.gameBoard[1] && versusComputer.spaces[6] == versusComputer.gameBoard[1]){
+                        return true
+                    }
+                }
+                if (versusComputer.spaces[8] == versusComputer.gameBoard[1]){
+                    if (versusComputer.spaces[7] == versusComputer.gameBoard[1] && versusComputer.spaces[6] == versusComputer.gameBoard[1]){
+                        return true
+                    }
+                }
+                if (versusComputer.spaces[3] == versusComputer.gameBoard[1]){
+                    if (versusComputer.spaces[4] == versusComputer.gameBoard[1] && versusComputer.spaces[5] == versusComputer.gameBoard[1]){
+                        return true
+                    }
+                }
+                if (versusComputer.spaces[1] == versusComputer.gameBoard[1]){
+                    if (versusComputer.spaces[4] == versusComputer.gameBoard[1] && versusComputer.spaces[7] == versusComputer.gameBoard[1]){
+                        return true
+                    }
+                }
+                }
+
+                if (computerWon()) {
+                    textDisplay.style.display = "none"
+                    versusComputer.announcer.style.visibility = "visible"
+                    versusComputer.announcer.textContent = "Computer has won!"
+                    return
+                }
+
+
+                /*
+               // minimax algo
+                const smartComputer = () => {
+            
+                    function emptyIndexes(board){
+                        return board.filter
+                    }
+                }*/
+
             })()
         }
-
-        let positions = ['0','1','2','3','4','5','6','7','8']
-    
-    
-         }
+     }
          btn2.addEventListener("click", againstComputer)
+
+         
          
     })()
 
     
-// code is a bit messy and needs to be cleaned up
-// commit this shit
-// code that makes computer the winner 
+
 
 
 
